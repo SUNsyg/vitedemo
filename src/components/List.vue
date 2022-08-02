1d63f215a01ec79f9210241502b163947492f5de
sunyinge <sunyinge@pxene.com>
2022-08-02 18:01:05 +0800

    '子传父'

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,26 +2,53 @@
  * @Description: 
  * @Author: Sun yinge
  * @Date: 2022-08-02 15:08:23
- * @LastEditTime: 2022-08-02 15:35:57
+ * @LastEditTime: 2022-08-02 17:54:04
  * @LastEditors: Sun yinge
 -->
 <template>
     <div>
-        这是子组件 ===> {{msg}}
+        这是子组件 ===> {{num}}
+        <button @click="changNum">按钮</button>
     </div>
 </template>
 
-<script setup>
-defineProps({
-    msg: {
-        type: String,
-        default: '111'
-    }
-})
+<script setup lang="ts">
+let num = ref(200);
+
+const emit = defineEmits<{
+  (e: 'fn', id: number): void
+}>()
+
+const changNum = ()=>{
+    emit('fn', num)
+}
 </script>
 
 <!-- <script type="text/javascript">
 export default {
-    props: ['msg']
+    setup(props, { emit }) {
+        let num = ref('100')
+        const changNum = ()=>{
+            emit('fn', num)
+        }
+        return {
+            num,
+            changNum
+        }
+    }
+}
+</script> -->
+<!-- <script type="text/javascript">
+export default {
+    data(){
+        return {
+            num: 10
+        }
+    },
+    methods: {
+        changNum() {
+            this.$emit('fn', this.num)
+        }
+    }
 }
 </script> -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,31 +2,57 @@
  * @Description: 
  * @Author: Sun yinge
  * @Date: 2022-07-28 15:04:44
- * @LastEditTime: 2022-08-02 15:25:46
+ * @LastEditTime: 2022-08-02 17:57:28
  * @LastEditors: Sun yinge
 -->
 <template>
   <div>
-    <List :msg="msg"></List>
+    <List :msg="msg" @fn="changeHome"></List>
   </div>
 </template>
 
 <script setup>
 import List from '../components/List.vue'
-let msg = ref('这是父传过去的数据')
+let changeHome = (n)=> {
+  console.log(n.value)
+}
 </script>
-
+<!-- <script type="text/javascript">
+import List from '../components/List.vue'
+export default {
+    components: {
+      List
+    },
+    setup() {
+        const msg = ref('1111111')
+        let changeHome = (n)=> {
+          console.log(n.value)
+        }
+        return {
+          msg,
+          changeHome
+        }
+    }
+}
+</script> -->
 <!-- <script type="text/javascript">
 import List from '../components/List.vue'
 
 export default {
   data() {
     return {
+      n:0,
       msg: '这是传过去的数据'
     }
   },
   components: {
     List
+  },
+  methods: {
+    changeHome(n) {
+      this.n = n;
+      console.log(n)
+    }
   }
 }
 </script> -->
