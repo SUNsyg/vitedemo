8f65d449ea1bf3c05516510ef8f20d1bbfcf14cd
sunyinge <sunyinge@pxene.com>
2022-08-03 18:02:03 +0800

    '暂存'

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,53 +2,27 @@
  * @Description: 
  * @Author: Sun yinge
  * @Date: 2022-08-02 15:08:23
- * @LastEditTime: 2022-08-02 17:54:04
+ * @LastEditTime: 2022-08-03 14:06:53
  * @LastEditors: Sun yinge
 -->
 <template>
     <div>
-        这是子组件 ===> {{num}}
-        <button @click="changNum">按钮</button>
+        <h1>这是List文件</h1>
+        <hr/>
+        子：{{num}}
+        <button @click="btn">按钮</button>
     </div>
 </template>
 
-<script setup lang="ts">
-let num = ref(200);
-
-const emit = defineEmits<{
-  (e: 'fn', id: number): void
-}>()
-
-const changNum = ()=>{
-    emit('fn', num)
+<script setup>
+let props = defineProps({
+    num:{
+        type:Number,
+        default:100
+    }
+})
+const emit = defineEmits(['update:num'])
+const btn = () => {
+    emit('update:num', 200)
 }
 </script>
-
-<!-- <script type="text/javascript">
-export default {
-    setup(props, { emit }) {
-        let num = ref('100')
-        const changNum = ()=>{
-            emit('fn', num)
-        }
-        return {
-            num,
-            changNum
-        }
-    }
-}
-</script> -->
-<!-- <script type="text/javascript">
-export default {
-    data(){
-        return {
-            num: 10
-        }
-    },
-    methods: {
-        changNum() {
-            this.$emit('fn', this.num)
-        }
-    }
-}
-</script> -->
